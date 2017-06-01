1dd7e3463a8b21d8a423d5c73e11d75e42ceca98
xjk <xujiankang@alphaun.com>
2017-06-02 00:14:46 +0800

    tomcat 学习计划

--- java/package-info.java
+++ java/package-info.java
@@ -2,6 +2,28 @@
  * http://blog.csdn.net/beliefer/article/details/51894747
  *
  * http://blog.csdn.net/c929833623lvcha/article/details/44677569
+ * http://blog.csdn.net/column/details/tomcat7-internal.html
+ *
+ * 学习计划
+ * 1. tomcat classLoader种类, 动态加载, 内存泄漏检测 (ClassLoaderLogManager, JreMemoryLeakPreventionListener, ThreadLocalLeakPreventionListener)
+ *
+ * 2. tomcat servlet jsp 原理(从启动加载, 路由映射, jsp动态编译成 servlet的class并且动态加载)
+ *
+ * 3. tomcat jasper 原理, 及处理过程
+ *
+ * 4. tomcat session 原理, 持久化
+ *
+ * 5. tomcat cookie 原理
+ *
+ * 6. tomcat 通信框架 coyote
+ *
+ * 7. tomcat 里面各种 valve
+ *
+ * 8. tomcat 里面各种 filter, 装饰器, 过滤器(以及其他 设计模式)
+ *
+ * 9. tomcat JMX 使用
+ *
+ * 10. tomcat 默认 bash 处理
  *
  * Created by xjk on 5/31/17.
  */
