510297453ed1efaf64e4bafdc2bc05ff3aafd598
xujiankang <xujiankang@zillionfortune.com>
2017-07-17 09:53:38 +0800

    add some comment for Tomcat Valve

---
+++ java/org/apache/catalina/session/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by xujiankang on 2017/7/17.
+ * 新增加了一个 Redis 来存储 Session
+ * 参考地址
+ * https://github.com/jcoleman/tomcat-redis-session-manager
+ */
+package org.apache.catalina.session;
