565321bad2b6305b95cf6f0015f1fcbf464de39f
xjk <xujiankang@alphaun.com>
2017-07-30 20:57:25 +0800

    add some comment for Tomcat

--- java/sketch/tomcat概要/tomcat启动.sql
+++ java/sketch/tomcat概要/tomcat启动.sql
@@ -7,15 +7,15 @@
 5. 加载 servlet, ServletContainerInitializer
 
 
-/usr/bin/java 																			# Java 命令														
--Djava.util.logging.config.file=${catalina.base}/conf/logging.properties 			    # Tomcat 的日志配置文件
--Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager 						# Tomcat 日志管理器	主要是读取日志的配置信息(能实现针对不同的 Context 配置不同的输出策略)	
--Djava.endorsed.dirs=/usr/local/tomcat7.0.73/endorsed 									# 这个参数是 JVM 提供给我们替换 jdk 原有类的一个功能(我的理解就是 BootstrapClassLoader 在进行加载class时, 会先加载这个目录下的类, 而忽视原先在 ${JDK}/lib 下的相同类名的类), 这里我们还想到 "java.ext.dirs" 通过这个参数我们可以给原有的 javaapi 增加一些新的功能
+/usr/bin/java 																			              # Java 命令
+-Djava.util.logging.config.file=${catalina.base}/conf/logging.properties 	# Tomcat 的日志配置文件
+-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager # Tomcat 日志管理器	主要是读取日志的配置信息(能实现针对不同的 Context 配置不同的输出策略)
+-Djava.endorsed.dirs=/usr/local/tomcat7.0.73/endorsed 						# 这个参数是 JVM 提供给我们替换 jdk 原有类的一个功能(我的理解就是 BootstrapClassLoader 在进行加载class时, 会先加载这个目录下的类, 而忽视原先在 ${JDK}/lib 下的相同类名的类), 这里我们还想到 "java.ext.dirs" 通过这个参数我们可以给原有的 javaapi 增加一些新的功能
 -classpath ${catalina.base}/bin/bootstrap.jar:${catalina.base}/bin/tomcat-juli.jar 		# classpath 路径, 一个是Tomcat 的启动引导包, 一个是Tomcat 自己的日志jar
 -Dcatalina.base=/usr/local/tomcat8.0.5 										
 -Dcatalina.home=/usr/local/tomcat7.0.5												    # Tomcat 的安装目录
 -Djava.io.tmpdir=/usr/local/tomcat8.0.5/temp 											# tomcat 的临时文件目录
-org.apache.catalina.startup.Bootstrap startup                                           # Java 程序 main 类
+org.apache.catalina.startup.Bootstrap startup                     # Java 程序 main 类
 
 
 
